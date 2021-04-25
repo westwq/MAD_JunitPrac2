1e0e4d90c03c396afd53fe60a5b0f0f8c9f57eb8
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-04-25 22:51:16 +0800

    Update Practice2Test.java

--- Practice2Test.java
+++ Practice2Test.java
@@ -1,1 +1,19 @@
 
+public class UserTest{
+    private User user;
+
+    @BeforeEach
+    public void setUp() throws Exception{
+        user = new User();
+        user.name = "test";
+        user.description = "testDes";;
+        user.id = 123;
+        user.followed = true;
+    }
+
+    @Test
+    public void testUser(){
+        
+    }
+
+}
