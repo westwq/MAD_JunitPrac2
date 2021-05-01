c73cd7a0c4f788f69279a1641069a16916fe61d1
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-05-01 10:47:12 +0800

    Update MainActivityTest.java

--- MainActivityTest.java
+++ MainActivityTest.java
@@ -1,27 +1,40 @@
 package sg.edu.np.mad.madpractical;
 
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
-@RunWith(JUnit4.class)
+
+@RunWith(AndroidJUnit4.class)
+
 public class MainActivityTest {
+
+    private String stringToBetyped;
+
     @Rule
-    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> activityRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
 
     @Test
-    public void pressImageView(){
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
         onView(withId(R.id.btnFollow)).perform(click());
+
+    }
+    
+    @After
+    public void tearDown() throws Exception {
     }
 }
