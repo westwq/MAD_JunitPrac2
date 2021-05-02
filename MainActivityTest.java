74b0083ddc4a0d0b6afcbc519288932cc1217f4f
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-05-02 12:13:42 +0800

    Update MainActivityTest.java

--- MainActivityTest.java
+++ MainActivityTest.java
@@ -1,5 +1,7 @@
 package sg.edu.np.mad.madpractical;
 
+import android.util.Log;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,14 +13,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 
 public class MainActivityTest {
-
-    private String stringToBetyped;
-
+    private final static String TAG = "Test Script";
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule
             = new ActivityScenarioRule<>(MainActivity.class);
@@ -31,7 +33,7 @@ public class MainActivityTest {
     public void changeText_sameActivity() {
         // Type text and then press the button.
         onView(withId(R.id.btnFollow)).perform(click());
-
+        onView(withId(R.id.btnFollow)).check(matches(isDisplayed()));
     }
     
     @After
