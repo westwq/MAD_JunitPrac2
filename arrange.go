654f42a6905de3bf7df60e2cb30a8abf10c13297
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-05-02 12:22:13 +0800

    Update arrange.go

--- arrange.go
+++ arrange.go
@@ -80,21 +80,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	from, err = os.Open("../testDirectory/MainActivityTest.java")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer from.Close()
-
-	to, err = os.OpenFile(realPath+"MainActivityTest.java", os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer to.Close()
-
-	_, err = io.Copy(to, from)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
